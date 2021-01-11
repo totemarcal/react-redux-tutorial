af2c331656fee53b0ebfef81783d1610c7c6c2a0
totemarcal <totemarcal@gmail.com>
2021-01-11 16:12:34 -0300

    redux

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return { articles: state.articles };
 };
 
